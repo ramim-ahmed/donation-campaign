0fc9b3e7ed049565a9ffde13546f60fa481af06f
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-03-25 05:35:17 +0600

    udpated

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -80,7 +80,7 @@ export default function Statistics() {
             fill="#8884d8"
             dataKey="value"
           >
-            {data.map((entry, index) => (
+            {data.map((_entry, index) => (
               <Cell
                 key={`cell-${index}`}
                 fill={COLORS[index % COLORS.length]}
